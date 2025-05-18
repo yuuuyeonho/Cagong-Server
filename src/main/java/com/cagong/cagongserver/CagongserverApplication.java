adfe5e4f005342329a0cb8b60f2eb4528e30aabb
YuYeonho <yuyeonho0619@gmail.com>
2025-05-18 22:36:01 +0900

    첫번째 커밋

---
+++ src/main/java/com/cagong/cagongserver/CagongserverApplication.java
@@ -0,0 +1,13 @@
+package com.cagong.cagongserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CagongserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CagongserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cagong/cagongserver/CagongserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cagong.cagongserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CagongserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
